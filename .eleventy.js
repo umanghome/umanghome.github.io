7c04c50172b194de8f028c19b3abbac95fa256c9
Umang Galaiya <umanghome@gmail.com>
2020-08-22 17:43:58 +0530

    Add 11ty config

---
+++ .eleventy.js
@@ -0,0 +1,40 @@
+const passthroughCopyItems = [
+  'css',
+  'img',
+  'manifest.json',
+];
+
+module.exports = config => {
+
+  // Copy /css to /site/css and other items
+  passthroughCopyItems.forEach(item => config.addPassthroughCopy(item));
+
+  config.addFilter('concat', function (a, b) {
+    return a.concat(b);
+  });
+
+  config.addFilter('allButPage', function (allPages, pageToOmit) {
+    return allPages.filter(page => page.url !== pageToOmit.url);
+  })
+
+  config.addCollection('pages', collectionApi => {
+    return collectionApi.getFilteredByGlob('src/*.html')
+      .sort((a, b) => {
+        if (a.data.navOrder < b.data.navOrder) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+  });
+
+  return {
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+
+    dir: {
+      input: 'src',
+      output: 'site',
+    },
+  }
+};
